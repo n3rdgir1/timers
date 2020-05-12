b392e017e73c1aafab2da0810bc7b68945184f15
n3rdgir1 <nerdgirl11235813@gmail.com>
2020-05-12 16:33:14 -0500

    master - niceties

---
+++ src/components/App/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <>
+      <header className="App-header">
+        <h1>Timers</h1>
+      </header>
+      <main />
+    </>
+  );
+}
+
+export default App;
